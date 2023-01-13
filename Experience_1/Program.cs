42bc90fb57813cfd6bbab9d469009e60d0d75623
Vutaly <Vutaly@DESKTOP-RSVQN6H>
2023-01-13 19:21:57 +0300

    Добавьте файлы проекта.

---
+++ Experience_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("The first number is greater than the second");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine("The first number is less than the second");
+            }
+            else
+            {
+                Console.WriteLine("Both numbers are equal");
+            }
+            Console.ReadKey();
+        }
+    }
+}
